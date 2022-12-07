73697f9e4261401a97b5f3c5338576e66ab5300d
MisterMAX789 <maximotruc@mail.ru>
2022-12-07 02:55:21 +0200

    first commit

---
+++ playwright/NetologyTest.js
@@ -0,0 +1,39 @@
+const { chromium } = require("playwright");
+const user = require("../playwright/user");
+const { test, expect } = require("@playwright/test");
+
+(async () => {
+	const browser = await chromium.launch({
+		headless: false,
+		slowMo: 5000,
+
+	});
+	const page = await browser.newPage();
+	await page.goto("https://netology.ru/?modal=sign_in");
+	await page.fill('[placeholder="Email"]', user.email);
+	await page.fill('[placeholder="Пароль"]', user.pass);
+	await page.click("text=Войти");
+
+	await expect(page).toHaveURL("https://netology.ru/profile");
+
+	await browser.close();
+})();
+
+(async () => {
+	const browser = await chromium.launch({
+		headless: false,
+		slowMo: 5000,
+
+	});
+	const page = await browser.newPage();
+	await page.goto("https://netology.ru/?modal=sign_in");
+	await page.fill('[placeholder="Email"]', "user@email.com");
+	await page.fill('[placeholder="Пароль"]', "user.pass");
+	await page.click("text=Войти");
+
+	await expect(
+		page.locator("text=Вы ввели неправильно логин или пароль")
+	).toBeVisible();
+
+	await browser.close();
+})();
---
+++ playwright/tests/user.js
@@ -0,0 +1,4 @@
+module.exports = {
+    email: "maximotruc-95@yandex.ru",
+    password:"A5jyzU2v5rHayJp("
+}
